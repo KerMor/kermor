398aff7faa2e163776acfd3ef06b24cda35880d6
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2011-05-13 14:15:25 +0200

    Introduced selection strategies for approx train data, finished RCLadder, speed improvements for ODE solving code

--- development.c
+++ development.c
@@ -10,7 +10,17 @@
  * @section codingguidelines Coding guidlines
  * Here you can find some information about how to write new code 
  * for the KerMor framework.
- * 
+ *
+ * @subsection create_files Creating new classes and functions 
+ * An extra class Devel has been created to aid @ref developers creating new files in the manner of KerMor.
+ *
+ * This class has two static methods Devel.newClass and Devel.newFun which can be used to create the 
+ * respective files in the current folder. When calling those functions, either the files template_class and template_fun
+ * from the KerMor root directory are parsed and their output is used as new file. This allows to automatically create 
+ * class/function skeletons matching the current developer and KerMor version.
+ *
+ * Please make sure you use the Devel class in order to keep the line of KerMor development.
+ *
  * @subsection getter_setter Getter & Setter
  * Writing getter & setter methods for your classes can usually be done straightforwardly.
  * As setters allow to check for a required type of a property, custom methods and
@@ -35,6 +45,7 @@
  * @par Further examples:
  * - dscomponents.AKernelCoreFun
  * - models.BaseModel
+ *
  *
  * @page developers KerMor Developers
  * @short Introduces the developers of KerMor
