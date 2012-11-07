94f44f690151a4a1ce27c0007c9f6fbd0cb62604
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2012-11-07 10:45:16 +0100

    started KerMor 0.7

--- newfeat_changelog.c
+++ newfeat_changelog.c
@@ -10,40 +10,47 @@
  *- @subpage newfeat0_4
  *- @subpage newfeat0_5
  *- @subpage newfeat0_6
+ *- @subpage newfeat0_7
  * 
  *@page newfeat0_1 New features in Version 0.1
  *@short Initial KerMor version before logging new features
  * 
- *Submitted to /agh/git/kermor repository on 08.03.2011
+ * Submitted to /agh/git/kermor repository on 08.03.2011
  * 
  *@page newfeat0_2 New features in Version 0.2
  *@short Created rbmatlab-support, time-scaling and a basic auto-config class to KerMor
  * 
- *Submitted to /agh/git/kermor repository on 22.03.2011
- *Branch name: master (no branches created before version 0.3)
+ * Submitted to /agh/git/kermor repository on 22.03.2011
+ * Branch name: master (no branches created before version 0.3)
  * 
  *@page newfeat0_3 New features in Version 0.3
  *@short Added RC circuit model, implemented @ref propclasses, improved error estimator computation
  * 
- *Submitted to /agh/git/kermor repository on 03.05.2011
- *Branch name: kermor03
+ * Submitted to /agh/git/kermor repository on 03.05.2011
+ * Branch name: kermor03
  *
  *@page newfeat0_4 New features in Version 0.4
  *@short Version for WH10 Experiments
  *
- *Submitted to /agh/git/kermor repository on 2011-06-20
- *Branch name: kermor04
+ * Submitted to /agh/git/kermor repository on 2011-06-20
+ * Branch name: kermor04
  * 
  *@page newfeat0_5 New features in Version 0.5
  *@short New visualization tools, some intermediate adaptive approx classes, new RealTimeMode for ODE solvers
  *
- *Submitted to /agh/git/kermor repository on 2011-11-09
- *Branch name: kermor05
- * 
+ * Submitted to /agh/git/kermor repository on 2011-11-09
+ * Branch name: kermor05
+ *
  *@page newfeat0_6 New features in Version 0.6
+ *@short New file-based large data structures, DEIM implementations with error estimators
+ *
+ * Submitted to /agh/git/kermor repository on 2012-11-07
+ * Branch name: kermor06
+ * 
+ *@page newfeat0_7 New features in Version 0.7
  *@short Under current development
  * 
- *Branch name: master
+ * Branch name: master
  * 
  */
 //################## Changelog page
@@ -59,38 +66,45 @@
  *- @subpage changelog0_4
  *- @subpage changelog0_5
  *- @subpage changelog0_6
+ *- @subpage changelog0_7
  *
  *@page changelog0_1 Changes in Version 0.1
  *@short Initial KerMor version before logging changes
  *
- *Submitted to /agh/git/kermor repository on 2011-03-08
+ * Submitted to /agh/git/kermor repository on 2011-03-08
  *
  *@page changelog0_2 Changes in Version 0.2
  *@short Some c implementation tests and stability/speedup fixes.
  *
- *Submitted to /agh/git/kermor repository on 2011-03-22
- *Branch name: master (no branches created before version 0.3)
+ * Submitted to /agh/git/kermor repository on 2011-03-22
+ * Branch name: master (no branches created before version 0.3)
  *
  *@page changelog0_3 Changes in Version 0.3
  *@short Added RC circuit model, implemented @ref propclasses, improved error estimator computation
  *
- *Submitted to /agh/git/kermor repository on 2011-05-03
- *Branch name: kermor03
+ * Submitted to /agh/git/kermor repository on 2011-05-03
+ * Branch name: kermor03
  *
  *@page changelog0_4 Changes in Version 0.4
  *@short Version for WH10 Experiments
  *
- *Submitted to /agh/git/kermor repository on 2011-06-20
- *Branch name: kermor04
+ * Submitted to /agh/git/kermor repository on 2011-06-20
+ * Branch name: kermor04
  *
  *@page changelog0_5 Changes in Version 0.5
  *@short Many bugfixes, most MUnit tests run, better class structure
  *
- *Submitted to /agh/git/kermor repository on 2011-11-09
- *Branch name: kermor05
- *
+ * Submitted to /agh/git/kermor repository on 2011-11-09
+ * Branch name: kermor05
+ * 
  *@page changelog0_6 Changes in Version 0.6
+ *@short Bugfixes, using inputParsers more often, more structure for dynamical systems components and direct ODEfun methods
+ *
+ * Submitted to /agh/git/kermor repository on 2012-11-07
+ * Branch name: kermor06
+ *
+ *@page changelog0_7 Changes in Version 0.7
  *@short Under current development
  *
- *Branch name: master
+ * Branch name: master
 */ 
