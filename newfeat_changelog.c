27b6c459e5aff1b43a5c5fc540b82440d802bbde
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2012-11-07 10:28:52 +0100

    complete walkthrough to fix documentation warnings (doxygen/mtoc++) new constructor of FileMatrix some new tests for testing.VKOGA fitted documentation/ files Commands.tex and Doxygen.template to fix latex errors and include references added some config defaults to mtocpp.conf fixed naming of newfeat/changelog sections according to new doxygen version smaller fixes to MatlabDocMaker (from mtoc++ project)

--- newfeat_changelog.c
+++ newfeat_changelog.c
@@ -4,43 +4,43 @@
  *@short The KerMor version new features log.
  * 
  *Here are all versions of KerMor and the corresponding changes:
- *- @subpage newfeat01
- *- @subpage newfeat02
- *- @subpage newfeat03
- *- @subpage newfeat04
- *- @subpage newfeat05
- *- @subpage newfeat06
+ *- @subpage newfeat0_1
+ *- @subpage newfeat0_2
+ *- @subpage newfeat0_3
+ *- @subpage newfeat0_4
+ *- @subpage newfeat0_5
+ *- @subpage newfeat0_6
  * 
- *@page newfeat01 New features in Version 0.1
+ *@page newfeat0_1 New features in Version 0.1
  *@short Initial KerMor version before logging new features
  * 
  *Submitted to /agh/git/kermor repository on 08.03.2011
  * 
- *@page newfeat02 New features in Version 0.2
+ *@page newfeat0_2 New features in Version 0.2
  *@short Created rbmatlab-support, time-scaling and a basic auto-config class to KerMor
  * 
  *Submitted to /agh/git/kermor repository on 22.03.2011
  *Branch name: master (no branches created before version 0.3)
  * 
- *@page newfeat03 New features in Version 0.3
+ *@page newfeat0_3 New features in Version 0.3
  *@short Added RC circuit model, implemented @ref propclasses, improved error estimator computation
  * 
  *Submitted to /agh/git/kermor repository on 03.05.2011
  *Branch name: kermor03
  *
- *@page newfeat04 New features in Version 0.4
+ *@page newfeat0_4 New features in Version 0.4
  *@short Version for WH10 Experiments
  *
  *Submitted to /agh/git/kermor repository on 2011-06-20
  *Branch name: kermor04
  * 
- *@page newfeat05 New features in Version 0.5
+ *@page newfeat0_5 New features in Version 0.5
  *@short New visualization tools, some intermediate adaptive approx classes, new RealTimeMode for ODE solvers
  *
  *Submitted to /agh/git/kermor repository on 2011-11-09
  *Branch name: kermor05
  * 
- *@page newfeat06 New features in Version 0.6
+ *@page newfeat0_6 New features in Version 0.6
  *@short Under current development
  * 
  *Branch name: master
@@ -53,43 +53,43 @@
  *@short The KerMor version changes log.
  *
  *Here are all versions of KerMor and the corresponding changes:
- *- @subpage changelog01
- *- @subpage changelog02
- *- @subpage changelog03
- *- @subpage changelog04
- *- @subpage changelog05
- *- @subpage changelog06
+ *- @subpage changelog0_1
+ *- @subpage changelog0_2
+ *- @subpage changelog0_3
+ *- @subpage changelog0_4
+ *- @subpage changelog0_5
+ *- @subpage changelog0_6
  *
- *@page changelog01 Changes in Version 0.1
+ *@page changelog0_1 Changes in Version 0.1
  *@short Initial KerMor version before logging changes
  *
  *Submitted to /agh/git/kermor repository on 2011-03-08
  *
- *@page changelog02 Changes in Version 0.2
+ *@page changelog0_2 Changes in Version 0.2
  *@short Some c implementation tests and stability/speedup fixes.
  *
  *Submitted to /agh/git/kermor repository on 2011-03-22
  *Branch name: master (no branches created before version 0.3)
  *
- *@page changelog03 Changes in Version 0.3
+ *@page changelog0_3 Changes in Version 0.3
  *@short Added RC circuit model, implemented @ref propclasses, improved error estimator computation
  *
  *Submitted to /agh/git/kermor repository on 2011-05-03
  *Branch name: kermor03
  *
- *@page changelog04 Changes in Version 0.4
+ *@page changelog0_4 Changes in Version 0.4
  *@short Version for WH10 Experiments
  *
  *Submitted to /agh/git/kermor repository on 2011-06-20
  *Branch name: kermor04
  *
- *@page changelog05 Changes in Version 0.5
+ *@page changelog0_5 Changes in Version 0.5
  *@short Many bugfixes, most MUnit tests run, better class structure
  *
  *Submitted to /agh/git/kermor repository on 2011-11-09
  *Branch name: kermor05
  *
- *@page changelog06 Changes in Version 0.6
+ *@page changelog0_6 Changes in Version 0.6
  *@short Under current development
  *
  *Branch name: master
